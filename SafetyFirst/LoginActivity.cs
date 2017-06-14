799d93b670fbe115395706310657450228000677
Tuan HoangDinh <Tuan.HoangDinh@studentpartner.com>
2017-06-15 03:06:52 +0700

    LoginUI

---
+++ SafetyFirst/LoginActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace SafetyFirst
+{
+    [Activity(Label = "LoginActivity", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class LoginActivity : AppCompatActivity
+    {
+        private Button btnLogin;
+        private EditText txtUser;
+        private EditText txtPass;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetTheme(Resource.Style.Theme_AppCompat_DayNight_NoActionBar);
+            base.OnCreate(savedInstanceState);
+            // Create your application here
+            SetContentView(Resource.Layout.LoginLayout);
+            txtUser = FindViewById<EditText>(Resource.Id.txtUser);
+            txtPass = FindViewById<EditText>(Resource.Id.txtPass);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+
+            txtPass.InputType = Android.Text.InputTypes.TextVariationPassword | Android.Text.InputTypes.ClassText;
+
+            btnLogin.Click += BtnLoginOnClick;
+        }
+
+
+        private void BtnLoginOnClick(object sender, EventArgs eventArgs)
+        {
+            Toast.MakeText(this, string.Format("User:{0}  Password:{1}", txtUser.Text, txtPass.Text), ToastLength.Short).Show();
+        }
+    }
+}
---
+++ SafetyFirst/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using GR.Net.Maroulis.Library;
+
+namespace SafetyFirst
+{
+    [Activity(Label = "SafetyFirst", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            var config = new EasySplashScreen(this)
+                .WithFullScreen()
+                .WithTargetActivity(Java.Lang.Class.FromType(typeof(LoginActivity)))
+                .WithSplashTimeOut(4000)
+                .WithBackgroundResource(Resource.Drawable.splashscreen);
+            View view = config.Create();
+            SetContentView(view);
+        }
+    }
+}
+
